2473f1949da5c67a3079f0f817d61eb9334e0bb6
chyroc <chyroc@qq.com>
2021-04-08 19:46:31 +0800

    test

--- p2p_test.go
+++ p2p_test.go
@@ -2,24 +2,36 @@ package ptr
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPointerInt64ToTime(t *testing.T) {
+	as := assert.New(t)
+
 	tests := []struct {
 		name string
 		args *int64
-		want [2]string
+		want []string
 	}{
 		{
 			name: "to-time-1617879700",
+			args: nil,
+			want: []string{},
+		},
+		{
+			name: "to-time-1617879700",
 			args: Int64(1617879700),
-			want: [2]string{"2021-04-08T19:01:40", "2021-04-08T11:01:40"},
+			want: []string{"2021-04-08T19:01:40", "2021-04-08T11:01:40"},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := PointerInt64ToTime(tt.args); got.Format("2006-01-02T15:04:05") != tt.want[0] && got.Format("2006-01-02T15:04:05") != tt.want[1] {
-				t.Errorf("PointerInt64ToTime() = %v, want %v", got.Format("2006-01-02T15:04:05"), tt.want)
+			got := PointerInt64ToTime(tt.args)
+			if len(tt.want) == 0 {
+				as.Nil(got)
+			} else {
+				as.Contains(tt.want, got.Format("2006-01-02T15:04:05"))
 			}
 		})
 	}
--- pointer_test.go
+++ pointer_test.go
@@ -1,515 +1,275 @@
 package ptr
 
 import (
-	"fmt"
-	"reflect"
+	"errors"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestBool(t *testing.T) {
+	as := assert.New(t)
+
 	vFalse := false
 	vTrue := true
-	tests := []struct {
-		name string
-		args bool
-		want *bool
-	}{
-		{
-			name: "bool-false",
-			args: false,
-			want: &vFalse,
-		},
-		{
-			name: "bool-true",
-			args: true,
-			want: &vTrue,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Bool(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Bool() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+
+	as.Equal(&vTrue, Bool(true))
+	as.Equal(&vFalse, Bool(false))
+	as.Equal((*bool)(nil), BoolNoNonePtr(false))
+	as.True(nil == BoolNoNonePtr(false))
+	as.Equal(&vTrue, BoolNoNonePtr(true))
 }
 
 func TestByte(t *testing.T) {
+	as := assert.New(t)
+
+	v0 := byte(0)
 	v1 := byte(1)
-	vS := byte('s')
-	tests := []struct {
-		name string
-		args byte
-		want *byte
-	}{
-		{
-			name: "byte-1",
-			args: byte(1),
-			want: &v1,
-		},
-		{
-			name: "byte-s",
-			args: 's',
-			want: &vS,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Byte(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Byte() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+
+	as.Equal(&v1, Byte(v1))
+	as.Equal((*byte)(nil), ByteNoNonePtr(v0))
+	as.Equal(&v1, ByteNoNonePtr(v1))
 }
 
 func TestComplex128(t *testing.T) {
+	as := assert.New(t)
+
+	v0 := complex(0, 0)
 	v1 := complex(1, 1)
-	v2 := complex(200, 200)
-	tests := []struct {
-		name string
-		args complex128
-		want *complex128
-	}{
-		{
-			name: "complex-1",
-			args: complex(1, 1),
-			want: &v1,
-		},
-		{
-			name: "complex-2",
-			args: complex(200, 200),
-			want: &v2,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Complex128(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Complex128() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+
+	as.Equal(&v0, Complex128(v0))
+	as.Equal(&v1, Complex128(v1))
+	as.Equal((*complex128)(nil), Complex128NoNonePtr(v0))
+	as.Equal(&v1, Complex128NoNonePtr(v1))
 }
 
 func TestComplex64(t *testing.T) {
+	as := assert.New(t)
+
+	v0 := complex(float32(0), float32(0))
 	v1 := complex(float32(1), float32(1))
-	tests := []struct {
-		name string
-		args complex64
-		want *complex64
-	}{
-		{
-			name: "complex-1",
-			args: v1,
-			want: &v1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Complex64(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Complex64() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+
+	as.Equal(&v0, Complex64(v0))
+	as.Equal(&v1, Complex64(v1))
+	as.Equal((*complex64)(nil), Complex64NoNonePtr(v0))
+	as.Equal(&v1, Complex64NoNonePtr(v1))
 }
 
 func TestDuration(t *testing.T) {
+	as := assert.New(t)
+
+	v0 := time.Duration(0)
 	v1 := time.Duration(1)
-	tests := []struct {
-		name string
-		args time.Duration
-		want *time.Duration
-	}{
-		{
-			name: "duration-1",
-			args: v1,
-			want: &v1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Duration(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Duration() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+
+	as.Equal(&v0, Duration(v0))
+	as.Equal(&v1, Duration(v1))
+	as.Equal((*time.Duration)(nil), DurationNoNonePtr(v0))
+	as.Equal(&v1, DurationNoNonePtr(v1))
 }
 
 func TestError(t *testing.T) {
-	v1 := fmt.Errorf("1")
-	tests := []struct {
-		name string
-		args error
-		want *error
-	}{
-		{
-			name: "err-nil",
-			args: nil,
-			want: nil,
-		},
-		{
-			name: "err-nil",
-			args: v1,
-			want: &v1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Error(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Error() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	as := assert.New(t)
+
+	v0 := (error)(nil)
+	v1 := errors.New("1")
+
+	as.Equal((*error)(nil), Error(v0))
+	as.Equal(&v1, Error(v1))
+	as.Equal((*error)(nil), ErrorNoNonePtr(v0))
+	as.Equal(&v1, ErrorNoNonePtr(v1))
 }
 
 func TestFloat32(t *testing.T) {
+	as := assert.New(t)
+
+	v0 := float32(0)
 	v1 := float32(1)
-	tests := []struct {
-		name string
-		args float32
-		want *float32
-	}{
-		{
-			name: "float-32",
-			args: v1,
-			want: &v1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Float32(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Float32() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+
+	as.Equal(&v0, Float32(v0))
+	as.Equal(&v1, Float32(v1))
+	as.Equal((*float32)(nil), Float32NoNonePtr(v0))
+	as.Equal(&v1, Float32NoNonePtr(v1))
 }
 
 func TestFloat64(t *testing.T) {
+	as := assert.New(t)
+
+	v0 := float64(0)
 	v1 := float64(1)
-	tests := []struct {
-		name string
-		args float64
-		want *float64
-	}{
-		{
-			name: "float-64",
-			args: v1,
-			want: &v1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Float64(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Float64() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+
+	as.Equal(&v0, Float64(v0))
+	as.Equal(&v1, Float64(v1))
+	as.Equal((*float64)(nil), Float64NoNonePtr(v0))
+	as.Equal(&v1, Float64NoNonePtr(v1))
 }
 
 func TestInt(t *testing.T) {
+	as := assert.New(t)
+
+	v0 := int(0)
 	v1 := int(1)
-	tests := []struct {
-		name string
-		args int
-		want *int
-	}{
-		{
-			name: "int-1",
-			args: v1,
-			want: &v1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Int(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Int() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
-func TestInt16(t *testing.T) {
-	v1 := int16(1)
-	tests := []struct {
-		name string
-		args int16
-		want *int16
-	}{
-		{
-			name: "int16-1",
-			args: v1,
-			want: &v1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Int16(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Int16() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestInt32(t *testing.T) {
-	v1 := int32(1)
-	tests := []struct {
-		name string
-		args int32
-		want *int32
-	}{
-		{
-			name: "int32-1",
-			args: v1,
-			want: &v1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Int32(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Int32() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestInt64(t *testing.T) {
-	v1 := int64(1)
-	tests := []struct {
-		name string
-		args int64
-		want *int64
-	}{
-		{
-			name: "int64-1",
-			args: v1,
-			want: &v1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Int64(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Int64() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	as.Equal(&v0, Int(v0))
+	as.Equal(&v1, Int(v1))
+	as.Equal((*int)(nil), IntNoNonePtr(v0))
+	as.Equal(&v1, IntNoNonePtr(v1))
 }
 
 func TestInt8(t *testing.T) {
+	as := assert.New(t)
+
+	v0 := int8(0)
 	v1 := int8(1)
-	tests := []struct {
-		name string
-		args int8
-		want *int8
-	}{
-		{
-			name: "int8-1",
-			args: v1,
-			want: &v1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Int8(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Int8() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+
+	as.Equal(&v0, Int8(v0))
+	as.Equal(&v1, Int8(v1))
+	as.Equal((*int8)(nil), Int8NoNonePtr(v0))
+	as.Equal(&v1, Int8NoNonePtr(v1))
+}
+
+func TestInt16(t *testing.T) {
+	as := assert.New(t)
+
+	v0 := int16(0)
+	v1 := int16(1)
+
+	as.Equal(&v0, Int16(v0))
+	as.Equal(&v1, Int16(v1))
+	as.Equal((*int16)(nil), Int16NoNonePtr(v0))
+	as.Equal(&v1, Int16NoNonePtr(v1))
+}
+
+func TestInt32(t *testing.T) {
+	as := assert.New(t)
+
+	v0 := int32(0)
+	v1 := int32(1)
+
+	as.Equal(&v0, Int32(v0))
+	as.Equal(&v1, Int32(v1))
+	as.Equal((*int32)(nil), Int32NoNonePtr(v0))
+	as.Equal(&v1, Int32NoNonePtr(v1))
+}
+
+func TestInt64(t *testing.T) {
+	as := assert.New(t)
+
+	v0 := int64(0)
+	v1 := int64(1)
+
+	as.Equal(&v0, Int64(v0))
+	as.Equal(&v1, Int64(v1))
+	as.Equal((*int64)(nil), Int64NoNonePtr(v0))
+	as.Equal(&v1, Int64NoNonePtr(v1))
 }
 
 func TestRune(t *testing.T) {
+	as := assert.New(t)
+
+	v0 := rune(0)
 	v1 := rune(1)
-	tests := []struct {
-		name string
-		args rune
-		want *rune
-	}{
-		{
-			name: "rune-1",
-			args: v1,
-			want: &v1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Rune(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Rune() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+
+	as.Equal(&v0, Rune(v0))
+	as.Equal(&v1, Rune(v1))
+	as.Equal((*rune)(nil), RuneNoNonePtr(v0))
+	as.Equal(&v1, RuneNoNonePtr(v1))
 }
 
 func TestString(t *testing.T) {
+	as := assert.New(t)
+
+	v0 := string("")
 	v1 := string("1")
-	tests := []struct {
-		name string
-		args string
-		want *string
-	}{
-		{
-			name: "string-1",
-			args: v1,
-			want: &v1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := String(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("String() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+
+	as.Equal(&v0, String(v0))
+	as.Equal(&v1, String(v1))
+	as.Equal((*string)(nil), StringNoNonePtr(v0))
+	as.Equal(&v1, StringNoNonePtr(v1))
 }
 
 func TestTime(t *testing.T) {
-	v1 := time.Now()
-	tests := []struct {
-		name string
-		args time.Time
-		want *time.Time
-	}{
-		{
-			name: "uint-1",
-			args: v1,
-			want: &v1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Time(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Time() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	as := assert.New(t)
+
+	v0 := time.Unix(0, 0)
+	v1 := time.Unix(1, 0)
+
+	as.Equal(&v0, Time(v0))
+	as.Equal(&v1, Time(v1))
+	as.Equal((*time.Time)(nil), TimeNoNonePtr(v0))
+	as.Equal(&v1, TimeNoNonePtr(v1))
 }
 
 func TestUInt(t *testing.T) {
+	as := assert.New(t)
+
+	v0 := uint(0)
 	v1 := uint(1)
-	tests := []struct {
-		name string
-		args uint
-		want *uint
-	}{
-		{
-			name: "uint-1",
-			args: v1,
-			want: &v1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := UInt(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("UInt() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+
+	as.Equal(&v0, UInt(v0))
+	as.Equal(&v1, UInt(v1))
+	as.Equal((*uint)(nil), UIntNoNonePtr(v0))
+	as.Equal(&v1, UIntNoNonePtr(v1))
 }
 
+
+func TestUInt8(t *testing.T) {
+	as := assert.New(t)
+
+	v0 := uint8(0)
+	v1 := uint8(1)
+
+	as.Equal(&v0, UInt8(v0))
+	as.Equal(&v1, UInt8(v1))
+	as.Equal((*uint8)(nil), UInt8NoNonePtr(v0))
+	as.Equal(&v1, UInt8NoNonePtr(v1))
+}
+
+
 func TestUInt16(t *testing.T) {
+	as := assert.New(t)
+
+	v0 := uint16(0)
 	v1 := uint16(1)
-	tests := []struct {
-		name string
-		args uint16
-		want *uint16
-	}{
-		{
-			name: "uint16-1",
-			args: v1,
-			want: &v1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := UInt16(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("UInt16() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+
+	as.Equal(&v0, UInt16(v0))
+	as.Equal(&v1, UInt16(v1))
+	as.Equal((*uint16)(nil), UInt16NoNonePtr(v0))
+	as.Equal(&v1, UInt16NoNonePtr(v1))
 }
 
 func TestUInt32(t *testing.T) {
+	as := assert.New(t)
+
+	v0 := uint32(0)
 	v1 := uint32(1)
-	tests := []struct {
-		name string
-		args uint32
-		want *uint32
-	}{
-		{
-			name: "uint32-1",
-			args: v1,
-			want: &v1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := UInt32(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("UInt32() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+
+	as.Equal(&v0, UInt32(v0))
+	as.Equal(&v1, UInt32(v1))
+	as.Equal((*uint32)(nil), UInt32NoNonePtr(v0))
+	as.Equal(&v1, UInt32NoNonePtr(v1))
 }
 
 func TestUInt64(t *testing.T) {
-	v1 := uint64(1)
-	tests := []struct {
-		name string
-		args uint64
-		want *uint64
-	}{
-		{
-			name: "uint64-1",
-			args: v1,
-			want: &v1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := UInt64(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("UInt64() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+	as := assert.New(t)
 
-func TestUInt8(t *testing.T) {
-	v1 := uint8(1)
-	tests := []struct {
-		name string
-		args uint8
-		want *uint8
-	}{
-		{
-			name: "uint8-1",
-			args: v1,
-			want: &v1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := UInt8(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("UInt8() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	v0 := uint64(0)
+	v1 := uint64(1)
+
+	as.Equal(&v0, UInt64(v0))
+	as.Equal(&v1, UInt64(v1))
+	as.Equal((*uint64)(nil), UInt64NoNonePtr(v0))
+	as.Equal(&v1, UInt64NoNonePtr(v1))
 }
 
 func TestUIntptr(t *testing.T) {
+	as := assert.New(t)
+
+	v0 := uintptr(0)
 	v1 := uintptr(1)
-	tests := []struct {
-		name string
-		args uintptr
-		want *uintptr
-	}{
-		{
-			name: "uintptr-1",
-			args: v1,
-			want: &v1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := UIntptr(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("UIntptr() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+
+	as.Equal(&v0, UIntptr(v0))
+	as.Equal(&v1, UIntptr(v1))
+	as.Equal((*uintptr)(nil), UIntptrNoNonePtr(v0))
+	as.Equal(&v1, UIntptrNoNonePtr(v1))
 }
